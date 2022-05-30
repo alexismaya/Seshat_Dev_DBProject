09fefa960712f71a2434362e519e4430f1359c31
alexismaya <alexis_dannymh@live.com.mx>
2022-05-30 15:19:12 -0500

    r

---
+++ client/src/pages/Factura.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Factura() {
+  return (
+    <StyledFactura>
+        
+    </StyledFactura>
+  )
+}
+
+const StyledFactura = styled.div`
+
+
+
+`
+
+export default Factura
