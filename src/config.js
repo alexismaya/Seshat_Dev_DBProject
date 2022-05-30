cae20cff85119f864b7ff50c824ad9ae2459b40a
alexismaya <alexis_dannymh@live.com.mx>
2022-05-30 13:41:48 -0500

    make it

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ config()
 
 module.exports = {
     bd: {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME
+        user: 'tjlgdbnhctfkkr',
+        password: '1bacf963a6793bf925e12692c0a6a75d6b93c2a0a31cbe357266575bcd751c77',
+        host: 'ec2-54-165-90-230.compute-1.amazonaws.com',
+        port: '5432',
+        database: 'dctt0c8uvtknps'
     }
 }
